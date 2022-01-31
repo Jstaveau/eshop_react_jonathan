0bf88c94b2c042089a8769bf99ca3f6064db3ecb
jonathan <jonathan.staveau@outlook.com>
2022-01-31 10:02:08 +0100

    js hm initialisation

---
+++ src/data/Produits.js
@@ -0,0 +1,141 @@
+import bounty from '../img/bounty.png'
+import cheetos from '../img/cheetos.png'
+import coca from '../img/coca.png'
+import dents from '../img/dents.png'
+import doritos from '../img/doritos.png'
+import dragibus from '../img/dragibus.png'
+import joris from '../img/joris.png'
+import lays from '../img/lays.png'
+import mw from '../img/mw.png'
+import pingui from '../img/pingui.png'
+import rings from '../img/rings.png'
+import sensation from '../img/sensation.png'
+import snikers from '../img/snikers.png'
+import tagada from '../img/tagada.png'
+import twix from '../img/twix.png'
+
+
+let produits =[
+    {
+        id: 1,
+        nom: 'Bounty',
+        categorie: 'chocolat',
+        prix: 2,
+        img: bounty,
+        stock: 20
+    },
+    {
+        id: 2,
+        nom: 'Cheetos',
+        categorie: 'chips',
+        prix: 1.3,
+        img: cheetos,
+        stock: 20
+    },
+    {
+        id: 3,
+        nom: 'Coca',
+        categorie: 'bonbons',
+        prix: 4.78,
+        img: coca,
+        stock: 20
+    },
+    {
+        id: 4,
+        nom: 'Dents de vampire',
+        categorie: 'bonbons',
+        prix: 4,
+        img: dents,
+        stock: 20
+    },
+    {
+        id: 5,
+        nom: 'Doritos nature',
+        categorie: 'chips',
+        prix: 0.4,
+        img: doritos,
+        stock: 10
+    },
+    {
+        id: 6,
+        nom: 'Dragibus',
+        categorie: 'bonbons',
+        prix: 4.34,
+        img: dragibus,
+        stock: 20
+    },
+    {
+        id: 7,
+        nom: 'Bonbon Joris',
+        categorie: 'bonbons',
+        prix: 6,
+        img: joris,
+        stock: 7
+    },
+    {
+        id: 8,
+        nom: 'Lays Ricetta Italiana',
+        categorie: 'chips',
+        prix: 2.73,
+        img: lays,
+        stock: 20
+    },
+    {
+        id: 9,
+        nom: 'Milky Way',
+        categorie: 'chocolat',
+        prix: 10,
+        img: mw,
+        stock: 20
+    },
+    {
+        id: 10,
+        nom: 'Kinder Pingui',
+        categorie: 'chocolat',
+        prix: 3.5,
+        img: pingui,
+        stock: 20
+    },
+    {
+        id: 11,
+        nom: 'Onion rings chips',
+        categorie: 'chips',
+        prix: 1.7,
+        img: rings,
+        stock: 20
+    },
+    {
+        id: 12,
+        nom: 'Lays sensation',
+        categorie: 'chips',
+        prix: 2,
+        img: sensation,
+        stock: 20
+    },
+    {
+        id: 13,
+        nom: 'Snikers',
+        categorie: 'chocolat',
+        prix: 2.2,
+        img: snikers,
+        stock: 20
+    },
+    {
+        id: 14,
+        nom: 'Fraises tagada',
+        categorie: 'bonbons',
+        prix: 3.4,
+        img: tagada,
+        stock: 20
+    },
+    {
+        id: 15,
+        nom: 'Twix',
+        categorie: 'chocolat',
+        prix: 2.7,
+        img: twix,
+        stock: 20
+    }
+]
+
+export default produits
