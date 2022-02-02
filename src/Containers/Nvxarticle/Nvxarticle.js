afe6d3a855bd993986a236cde0f2aa14f828a251
jonathan <jonathan.staveau@outlook.com>
2022-02-02 16:04:37 +0100

    js hm pq OBIC AQBC GPT

--- src/Containers/Nvxarticle/Nvxarticle.js
+++ src/Containers/Nvxarticle/Nvxarticle.js
@@ -66,22 +66,22 @@ export default function Nvxarticle() {
             prevent(e)
             changeToggle()
         }} className='container d-flex flex-column align-items-start'>
-            <div className='d-flex align-items-center justify-content-center w-100'>
-                <div className='w-50 m-2 d-flex flex-column'>
+            <div className='d-flex flex-column flex-sm-row align-items-center justify-content-between w-100'>
+                <div className='col-sm-5 col-10 d-flex flex-column'>
                     <label className='fw-bold mt-3' htmlFor="nom">Nom article</label>
                     <input onChange={nomArticle} type="text" id='nom' required="required"/>
                 </div>
-                <div className='w-50 m-2 d-flex flex-column'>
+                <div className='col-sm-5 col-10 d-flex flex-column'>
                     <label className='fw-bold mt-3' htmlFor="prix">Prix article (en €)</label>
                     <input onChange={prixArticle} type="number" id='prix' required="required"/>
                 </div>
             </div>
-            <div className='d-flex align-items-center justify-content-center w-100'>
-                <div className='w-50 m-2 d-flex flex-column'>
+            <div className='d-flex flex-column flex-sm-row align-items-center justify-content-between w-100'>
+                <div className='col-sm-5 col-10 d-flex flex-column'>
                     <label className='fw-bold mt-3' htmlFor="stock">Stock</label>
                     <input onChange={stockArticle} type="number" id='stock' required="required"/>
                 </div>
-                <div className='w-50 m-2 d-flex flex-column'>
+                <div className='col-sm-5 col-10 d-flex flex-column'>
                     <label className='fw-bold mt-3' htmlFor="categorie">Choisissez une catégorie</label>
                     <select onChange={categorieArticle} name="categorie" id="categorie" required="required">
                         <option value="chocolats">Chocolats</option>
@@ -90,7 +90,7 @@ export default function Nvxarticle() {
                     </select>
                 </div>
             </div>
-            <div className='w-50 m-2 d-flex flex-column'>
+            <div className='col-12 col-sm-5 mb-3 d-flex flex-column'>
                 <label className='fw-bold mt-3' htmlFor="image">Choisissez une image pour votre article</label>
                 <input onChange={imgArticle} type="url" id='image' required="required"/>
             </div>
